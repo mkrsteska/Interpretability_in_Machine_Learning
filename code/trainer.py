f98af6357b4ebfef085af4ff07a5fd5d3a8ddbfc
mkrsteska <marija.krsteska@unil.ch>
2020-08-26 23:14:17 +0200

    Add accuracy heatmaps

--- code/trainer.py
+++ code/trainer.py
@@ -10,6 +10,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 from ast import literal_eval as make_tuple
 
 class Trainer():
@@ -33,6 +34,9 @@ class Trainer():
         Name of student models. Used for plotting the results.
         
     model_predictions : list
+        Contains list of predictions from each model.
+    
+    test_predictions : list
         Contains list of predictions from each model.
         
     reference_model_y_test_predictions : list 
@@ -82,8 +86,14 @@ class Trainer():
         
         print("Plotting the predictions for a sample of X_train set")
         
-        self.model_predictions = []     
-        self.predict(self.data_loader.X_train, self.data_loader.y_train)        
+        self.model_predictions = []   
+        self.test_predictions = []
+        self.predict(self.data_loader.X_train, self.data_loader.y_train, True)  
+        
+        if(self.data_loader.separate_train_test_file == "False"):            
+            self.initial_test_accuracy = self.calculate_test_accuracy()
+        self.initial_train_accuracy = self.calculate_train_accuracy()
+
         self.matrix1 = self.create_confusion_matrix()
         
         print("Plotting the predictions for a sample of X_train + more labeled points given by the reference model")
@@ -96,7 +106,8 @@ class Trainer():
         
         self.model_predictions = []      
         self.predict(self.X_train_X_test, self.y_train_y_test)
-        self.matrix2 = self.create_confusion_matrix()
+        
+        self.matrix2 = self.create_confusion_matrix()        
         
         print("Plotting heatmaps")
         self.plot_confusion_matrices()
@@ -151,7 +162,25 @@ class Trainer():
         except:
             return False
     
-    def predict(self, X_train, y_train):
+    def calculate_test_accuracy(self):
+        """
+        Calculates initial test accuracy of every model.
+        """
+        accuracy = []
+        for pred in self.test_predictions:
+            accuracy.append(accuracy_score(self.data_loader.y_test, pred))
+        return accuracy
+
+    def calculate_train_accuracy(self):
+        """
+        Calculates initial train accuracy of every model.
+        """
+        accuracy = []
+        for pred in self.model_predictions:
+            accuracy.append(accuracy_score(self.data_loader.y_train, pred))
+        return accuracy     
+    
+    def predict(self, X_train, y_train, test_predict=False):
         """
         Predict the target labels with each model, store the predictions in `self.model_predictions` and plot a sample of the predictions.
 
@@ -162,7 +191,9 @@ class Trainer():
 
         y_train : pandas.Series, required
             The output train dataset. Contains the target label for every instance in `X_train`.
-
+        
+        test_predict: bool, optional
+            Whether to generate predictions on the original test dataset.
         """
         self.plot_sample = self.data_loader.get_plot_sample(X_train, y_train)
         self.number = int(math.sqrt(self.data_loader.square_number))
@@ -170,6 +201,9 @@ class Trainer():
         for self.model in self.models:
             self.model.fit(X_train, y_train)
             self.model_predictions.append(self.model.predict(X_train))
+            
+            if test_predict:
+                self.test_predictions.append(self.model.predict(self.data_loader.X_test))
             
             self.blend_sample_predictions()
             self.plot_sample_predictions() 
@@ -237,16 +271,55 @@ class Trainer():
         """
         Plots heatmaps using the normalized values from `self.matrix1` and `self.matrix2`. Additionally, plots a heatmap with the relative improvement in matching predictions for each pair of models when using the augmented dataset.  
         """
+        if(self.data_loader.separate_train_test_file == "True"):
+            print("\n Test accuracy can not be calculated")
+        else: 
+            plt.figure(figsize=(12, 2))
+            plt.title("Accuracy of each model on the initial test set")
+            accuracy = {k:v for (k, v) in zip(self.labels, self.initial_test_accuracy)}
+            accuracy = sorted(accuracy.items(), key=lambda x: x[1], reverse=True)
+            values_ordered = [x[1] for x in accuracy]
+            models_ordered = [x[0] for x in accuracy]
+            seaborn.heatmap([values_ordered], xticklabels=models_ordered, yticklabels=[""],cmap='Reds', annot=True, fmt='g', cbar=False)
+            
+        plt.figure(figsize=(12, 2))
+        plt.title("Accuracy of each model on the initial train set")
+        accuracy = {k:v for (k, v) in zip(self.labels, self.initial_train_accuracy)}
+        accuracy = sorted(accuracy.items(), key=lambda x: x[1], reverse=True)
+        values_ordered = [x[1] for x in accuracy]
+        models_ordered = [x[0] for x in accuracy]
+        seaborn.heatmap([values_ordered], xticklabels=models_ordered, yticklabels=[""],cmap='Reds', annot=True, fmt='g', cbar=False) 
         
         plt.figure(figsize=(16, 12))
         plt.subplot(211)
-        plt.title("Percentage of matching predictions for each pair of models for the initial test set")
-        seaborn.heatmap(self.matrix1 / self.matrix1.max() * 100, xticklabels = self.labels, yticklabels = self.labels, cmap='Blues', annot=True, fmt='g')
+        plt.title("Percentage of matching predictions for each pair of models for the initial train set")
+        ax = seaborn.heatmap(self.matrix1 / self.matrix1.max() * 100, xticklabels = self.labels, yticklabels = self.labels, cmap='Blues', annot=True, fmt='g')
+        self.highlight_reference_row(ax)
         
         plt.subplot(212)
-        plt.title("Percentage of matching predictions for each pair of models when using the augmented dataset")
-        seaborn.heatmap(self.matrix2 / self.matrix2.max() * 100, xticklabels = self.labels, yticklabels = self.labels, cmap='Blues', annot=True, fmt='g')
+        plt.title("Percentage of matching predictions for each pair of models when using the union dataset")
+        ax = seaborn.heatmap(self.matrix2 / self.matrix2.max() * 100, xticklabels = self.labels, yticklabels = self.labels, cmap='Blues', annot=True, fmt='g')
+        self.highlight_reference_row(ax)
         
-        plt.figure(figsize=(10, 6))
+        plt.figure(figsize=(16, 5))
         plt.title('Relative improvement in matching predictions')
-        seaborn.heatmap(self.matrix2 / self.matrix2.max() * 100 - self.matrix1 / self.matrix1.max() * 100, xticklabels = self.labels, yticklabels = self.labels, cmap='Blues', annot=True, fmt='g')
+        ax = seaborn.heatmap(self.matrix2 / self.matrix2.max() * 100 - self.matrix1 / self.matrix1.max() * 100, xticklabels = self.labels, yticklabels = self.labels, cmap='Blues', annot=True, fmt='g')
+        self.highlight_reference_row(ax)
+        
+    def highlight_reference_row(self, ax):
+        """
+        Highlights the first row in the heatmap.
+        
+        Parameters
+        ----------
+        ax : required         
+        """
+        for lab in ax.get_yticklabels():
+            text =  lab.get_text()
+            if text == self.reference_model.__class__.__name__: 
+                lab.set_weight('bold')
+                lab.set_size(15)
+                lab.set_color('red')
+        for annot in ax.texts[:len(self.models)]:
+            annot.set_weight('bold')
+            annot.set_size(15)
